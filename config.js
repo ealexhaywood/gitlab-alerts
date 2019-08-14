4cab1e50638c7eea5238088687b42c1b8f2ceb92
E. Alex Haywood <ealexhaywood@gmail.com>
2019-08-14 09:47:02 -0700

    Fix npm vulns

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ config.gitlab.url = process.env.GITLAB_URL || 'http://localhost:30080';
 config.gitlab.api_prefix = process.env.GITLAB_API_PREFIX || '/api/v4/projects/';
 // See https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html#personal-access-tokens
 config.gitlab.access_token = process.env.ACCESS_TOKEN || '6vvgXdrsTBHevctUSxSK';
-config.gitlab.id = process.env.GITLAB_ID || '1';
+config.gitlab.id = process.env.PROJECT_ID || '1';
 config.gitlab.iid = process.env.GITLAB_IID || '';
 config.gitlab.confidential = process.env.GITLAB_CONFIDENTIAL || false;
 config.gitlab.assignee_ids = process.env.GITLAB_ASSIGNEE_IDS || [];
